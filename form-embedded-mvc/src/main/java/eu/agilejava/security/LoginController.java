2d08c55de129b2302c2339fc5e73f503eec2c81e
Ivar Grimstad <ivar.grimstad@gmail.com>
2016-03-14 08:42:59 +0100

    MVC Form based auth WIP

---
+++ form-embedded-mvc/src/main/java/eu/agilejava/security/LoginController.java
@@ -0,0 +1,53 @@
+package eu.agilejava.security;
+
+import javax.inject.Inject;
+import javax.mvc.Models;
+import javax.mvc.annotation.Controller;
+import javax.mvc.annotation.View;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Ivar Grimstad (ivar.grimstad@gmail.com).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/**
+ *
+ * @author Ivar Grimstad (ivar.grimstad@gmail.com)
+ */
+@Path("login")
+@Controller
+public class LoginController {
+
+    @Inject
+    private Models models;
+    
+    @GET
+    @View("login.jsp")
+    public void loginForm(@QueryParam("auth") int status) {
+
+        if(status == -1) {
+            models.put("msg", "login failed");
+        }
+    }
+}
---
+++ form-embedded-mvc/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,28 @@
+<%-- 
+    Document   : hello.jsp
+    Created on : Feb 11, 2016, 2:23:16 PM
+    Author     : Ivar Grimstad (ivar.grimstad@gmail.com)
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Soteria MVC</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        
+        Web username: <b>${user}</b><br/>
+        Web user has role "foo": <b>${hasFoo}</b><br/>
+        Web user has role "bar": <b>${hasBar}</b><br/>
+        Web user has role "kaz": <b>${hasKaz}</b><br/>
+        
+        <form name="form" action="${mvc.basePath}/login/logout" method="post">
+            <p>
+                <input type="submit" value="Logout" name="button"/>
+            </p>
+        </form>
+    </body>
+</html>
