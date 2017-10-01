0a85bf2299c87ca21af58c98521094eba3879f0a
Ivar Grimstad <ivar.grimstad@gmail.com>
2017-10-01 11:24:10 -0700

    Adjust demo to Java EE 8

--- simple-jsf/src/main/java/eu/agilejava/security/SimpleJSFAuthenticationMechanism.java
+++ simple-jsf/src/main/java/eu/agilejava/security/SimpleJSFAuthenticationMechanism.java
@@ -42,33 +42,23 @@ import javax.servlet.http.HttpServletResponse;
 @DeclareRoles({"foo", "bar", "kaz"})
 @AutoApplySession
 @RequestScoped
-public class JSFAuthenticationMechanism implements HttpAuthenticationMechanism {
-
+public class SimpleJSFAuthenticationMechanism implements HttpAuthenticationMechanism {
+    
     @Inject
     private IdentityStore identityStore;
 
     @Override
     public AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) throws AuthenticationException {
 
-        if (request.getParameter("j_idt6:j_idt9") != null && request.getParameter("j_idt6:j_idt11") != null) {
+        if (request.getParameter("login:username") != null && request.getParameter("login:password") != null) {
 
-            // Get the (caller) name and password from the request
-            // NOTE: This is for the smallest possible example only. In practice
-            // putting the password in a request query parameter is highly
-            // insecure
-            String name = request.getParameter("j_idt6:j_idt9");
-            Password password = new Password(request.getParameter("j_idt6:j_idt11"));
+            String name = request.getParameter("login:username");
+            Password password = new Password(request.getParameter("login:password"));
 
-            // Delegate the {credentials in -> identity data out} function to
-            // the Identity Store
             CredentialValidationResult result = identityStore.validate(
                     new UsernamePasswordCredential(name, password));
 
             if (result.getStatus() == VALID) {
-                // Communicate the details of the authenticated user to the
-                // container. In many cases the underlying handler will just store the details 
-                // and the container will actually handle the login after we return from 
-                // this method.
                 return httpMessageContext.notifyContainerAboutLogin(
                         result.getCallerPrincipal(), result.getCallerGroups());
             } else {
