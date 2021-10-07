551014be02408226842a21774be32daa05148253
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2021-10-07 14:19:04 +0200

    live template

--- simple-rest/src/main/java/eu/agilejava/security/SimpleAuthenticationMechanism.java
+++ simple-rest/src/main/java/eu/agilejava/security/SimpleAuthenticationMechanism.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2016 Ivar Grimstad (ivar.grimstad@gmail.com).
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package eu.agilejava.security;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -32,32 +9,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.HashSet;
-import java.util.logging.Logger;
 
 import static java.util.Arrays.asList;
 
 @ApplicationScoped
 public class SimpleAuthenticationMechanism implements HttpAuthenticationMechanism {
 
-    private static final Logger LOGGER = Logger.getLogger("eu.agilejava.dukes");
-
     @Override
     public AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) throws AuthenticationException {
 
         if (!request.getRequestURI().contains("protected")) {
-            LOGGER.warning(() -> "Do nothing");
             return httpMessageContext.doNothing();
         }
 
         final String key = request.getHeader("MY-API-KEY");
         if (key != null && key.equalsIgnoreCase("DUKE ROCKS")) {
-
-            LOGGER.warning(() -> "Everything fine!");
             return httpMessageContext.notifyContainerAboutLogin(
                     "app", new HashSet<>(asList("foo")));
         }
 
-        LOGGER.warning(() -> "Unauthorized");
         return httpMessageContext.responseUnauthorized();
     }
 }
